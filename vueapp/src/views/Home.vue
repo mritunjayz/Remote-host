1f2a96ea041a5efe5be5629dde3f6476a83061c0
mritunjaygoutam12 <mritunjaygoutam2204@gmail.com>
2019-01-07 13:01:32 +0530

    login component added in vue

---
+++ vueapp/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <HelloWorld msg="Welcome to Your Remote Desk"/>
+    <login/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import login from '@/components/login.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld,
+    login
+  }
+}
+</script>
